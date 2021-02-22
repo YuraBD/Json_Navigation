b02904fd80b107d8ec5713009b3ad1507a3a9ee6
Yurii Bodakovskyi <yurii.bodakovskyi@ucu.edu.ua>
2021-02-22 22:10:11 +0200

    Program works

---
+++ json_navigation.py
@@ -0,0 +1,92 @@
+import requests
+import pprint
+import json
+
+
+def make_json(user, friends_number, bearer_token):
+    base_url = "https://api.twitter.com/"
+    search_url = f"{base_url}1.1/friends/list.json"
+    search_headers = {
+        "Authorization": f"Bearer {bearer_token}"
+    }
+    search_params = {
+        'screen_name': user,
+        'count': friends_number
+    }
+    response = requests.get(search_url, headers=search_headers, params=search_params)
+    json_response = response.json()
+    return json_response
+
+
+def navigate(obj, depth = 0):
+    while True:
+        if isinstance(obj, dict):
+            if depth != 0:
+                print("This is dictionary. Enter a key index from list below or -1 to return back, -2 to end a program: ")
+            else:
+                print("This is dictionary. Enter a key index from list below or -2 to end a program: ")
+            keys = list(obj.keys())
+            for i in range(len(keys)):
+                if i == len(keys) - 1:
+                    print(f'{i}) {keys[i]}')
+                    break
+                print(f'{i}) {keys[i]}   ', end='')
+            ind = int(input('Key index: '))
+            if ind == -2:
+                print("Do you want to end a program?")
+                print("0) Yes   1) No")
+                if int(input("Choose an option: ")) == 0:
+                    return 0
+            elif ind == -1:
+                return 1
+            else:
+                if not navigate(obj[keys[ind]], 1):
+                    return 0
+        elif isinstance(obj, list):
+            print("This is list. Do you want to display the entire list?")
+            print("0) Yes   1) No")
+            display = int(input('Choose an option: '))
+            if display == 1:
+                pass
+            else:
+                for i in range(len(obj)):
+                    if i == len(obj) - 1:
+                        print(f'{i}) {obj[i]}')
+                        break
+                    print(f'{i}) {obj[i]}   ', end='')
+            print(f'Enter an index in range from 0 to {len(obj)-1} or -1 to return back, -2 to end a program')
+            ind = int(input("Enter an index: "))
+            if ind == -2:
+                print("Do you want to end a program?")
+                print("0) Yes   1) No")
+                if int(input("Choose an option: ")) == 0:
+                    return 0
+            elif ind == -1:
+                return 1
+            else:
+                if not navigate(obj[ind], 1):
+                    return 0
+        else:
+            obj_type = str(type(obj)).split('\'')[1]
+            print(f'This is {obj_type}. Do you want to display it?')
+            print('0) Yes   1) No')
+            if int(input('Choose an option: ')) == 0:
+                print('\n' ,obj, '\n')
+            print('This is the end of the file.')
+            print('Enter -1 to return back or -2 to end a program')
+            if int(input('>>> ')) == -1:
+                return 1
+            else:
+                return 0 
+
+
+def main():
+    user = input('Enter username: ')
+    friend_number = int(input('Enter number of friends to show: '))
+    bearer_token = input('Enter bearer token: ')
+    friends_json = make_json(user, friend_number, bearer_token)
+    navigate(friends_json)
+
+
+if __name__ == "__main__":
+    main()
